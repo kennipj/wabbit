ed642c1bb590cd65c9220b8cf2958c80f2f79d69
Kenni Jørgensen <kenni@argyle.com>
2024-01-16 13:09:27 +0000

    improve format readability

--- wab/format.py
+++ wab/format.py
@@ -20,6 +20,13 @@ from wab.model import (
 )
 
 
+def format_program(program: Program) -> str:
+    lines = Lines()
+    for stmt in program.statements:
+        fmt_stmt(stmt, lines)
+    return "\n".join(lines)
+
+
 class Lines(list[str]):
     def __init__(self):
         super().__init__()
@@ -40,61 +47,67 @@ class Lines(list[str]):
 
 def fmt_expr(node: Expression) -> str:
     match node:
-        case Name():
-            return node.value
-        case Integer():
-            return str(node.value)
-        case BinOp():
-            return f"{fmt_expr(node.lhs)} {node.op} {fmt_expr(node.rhs)}"
-        case Parenthesis():
-            return f"({fmt_expr(node.expr)})"
-        case Call():
-            args = ", ".join(fmt_expr(arg) for arg in node.args)
-            return f"{fmt_expr(node.name)}({args})"
+        case Name(value):
+            return value
+
+        case Integer(value):
+            return str(value)
+
+        case BinOp(op, lhs, rhs):
+            return f"{fmt_expr(lhs)} {op} {fmt_expr(rhs)}"
+
+        case Parenthesis(expr):
+            return f"({fmt_expr(expr)})"
+
+        case Call(name, args):
+            formatted_args = ", ".join(fmt_expr(arg) for arg in args)
+            return f"{fmt_expr(name)}({formatted_args})"
+
         case _:
             raise ValueError(f"Unexpected expression: {node}")
 
 
 def fmt_stmt(node: Statement, lines: Lines) -> None:
     match node:
-        case Assignment():
-            lines.append(f"{fmt_expr(node.lhs)} = {fmt_expr(node.rhs)};")
-        case Variable():
-            lines.append(f"var {fmt_expr(node.name)} = {fmt_expr(node.expr)};")
-        case VariableDecl():
-            lines.append(f"var {fmt_expr(node.name)};")
-        case Print():
-            lines.append(f"print {fmt_expr(node.expr)};")
-        case While():
-            lines.append(f"while {fmt_expr(node.condition)} {{")
-            with lines.indent():
-                any(fmt_stmt(stmt, lines) for stmt in node.body)
-            lines.append("}")
-        case Branch():
-            lines.append(f"if {fmt_expr(node.condition)} {{")
-            with lines.indent():
-                any(fmt_stmt(stmt, lines) for stmt in node.body)
+        case Assignment(lhs, rhs):
+            lines.append(f"{fmt_expr(lhs)} = {fmt_expr(rhs)};")
 
-            if node.else_:
+        case Variable(name, expr):
+            lines.append(f"var {fmt_expr(name)} = {fmt_expr(expr)};")
+
+        case VariableDecl(name):
+            lines.append(f"var {fmt_expr(name)};")
+
+        case Print(expr):
+            lines.append(f"print {fmt_expr(expr)};")
+
+        case While(condition, body):
+            lines.append(f"while {fmt_expr(condition)} {{")
+            with lines.indent():
+                any(fmt_stmt(stmt, lines) for stmt in body)
+            lines.append("}")
+
+        case Branch(condition, body, else_):
+            lines.append(f"if {fmt_expr(condition)} {{")
+            with lines.indent():
+                any(fmt_stmt(stmt, lines) for stmt in body)
+
+            if else_:
                 lines.append("} else {")
                 with lines.indent():
-                    any(fmt_stmt(stmt, lines) for stmt in node.else_)
+                    any(fmt_stmt(stmt, lines) for stmt in else_)
 
             lines.append("}")
-        case Function():
-            args = ", ".join(fmt_expr(arg) for arg in node.args)
-            lines.append(f"func {fmt_expr(node.name)}({args}) {{")
+
+        case Function(name, args):
+            formatted_args = ", ".join(fmt_expr(arg) for arg in args)
+            lines.append(f"func {fmt_expr(name)}({formatted_args}) {{")
             with lines.indent():
                 any(fmt_stmt(stmt, lines) for stmt in node.body)
             lines.append("}")
-        case Return():
-            lines.append(f"return {fmt_expr(node.expr)};")
+
+        case Return(expr):
+            lines.append(f"return {fmt_expr(expr)};")
+
         case _:
             raise ValueError(f"Unexpected statement: {node}")
-
-
-def format_program(program: Program) -> str:
-    lines = Lines()
-    for stmt in program.statements:
-        fmt_stmt(stmt, lines)
-    return "\n".join(lines)
